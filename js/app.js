fa9128e5d7140bd211ebcfc0acdd2ce7ce37ec3d
batman <mittal.anant@gmail.com>
2016-06-07 09:24:33 +0530

    Code gives us read and write quantitative assessment

--- js/app.js
+++ js/app.js
@@ -1,80 +1,138 @@
-angular.module('myApp', [])
-	.controller('MainCtrl', function ($scope, $timeout, $http) {
-		// Build the date object
-		$scope.date = {};
+angular
+		.module('myApp', [])
+		.controller(
+				'MainCtrl',
+				function($scope, $timeout, $http) {
+					// Build the date object
+					// $scope.date = {};
+					// Update function
+					/*
+					 * var updateTime = function () { $scope.date.raw = new
+					 * Date(); $timeout(updateTime, 1000); }
+					 */
 
-		// Update function
-		var updateTime = function () {
-			$scope.date.raw = new Date();
-			$timeout(updateTime, 1000);
-		}
-		
-		$http.get('http://apps.swasth.org/shc/index.php/myswasth/myswasth/get_personids_from_cardnumber/10').success(function (data) {
-			console.log(data);
-			window.IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction || window.msIDBTransaction;
-			window.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange || window.msIDBKeyRange
+					window.IDBTransaction = window.IDBTransaction
+							|| window.webkitIDBTransaction
+							|| window.msIDBTransaction;
+					window.IDBKeyRange = window.IDBKeyRange
+							|| window.webkitIDBKeyRange || window.msIDBKeyRange
 
-			if (!window.indexedDB) {
-				window.alert("Your browser doesn't support a stable version of IndexedDB.")
-			}else{
-				var indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB || window.shimIndexedDB;
+					if (!window.indexedDB) {
+						window
+								.alert("Your browser doesn't support a stable version of IndexedDB.")
+					} else {
+						var indexedDB = window.indexedDB || window.mozIndexedDB
+								|| window.webkitIndexedDB || window.msIndexedDB
+								|| window.shimIndexedDB;
 
-				// Open (or create) the database
-				var open = indexedDB.open("MyDatabase", 1);
+						var req = indexedDB.deleteDatabase("MyDatabase");
+					}
 
-				// Create the schema
-				open.onupgradeneeded = function() {
-					console.log("Onupgraded");
-				    var db = open.result;
-				    var store = db.createObjectStore("MyObjectStore", {keyPath: "id"});
-				    var index = store.createIndex("NameIndex", ["name", "gender"]);
-				};
+					$scope.readlat = function() {
+						//console.log("Read");
+						var open = indexedDB.open("MyDatabase", 1);
+						open.onsuccess = function() {
+							//console.log("Onsuccess");
+								var db = open.result;
+								var tx = db.transaction("MyObjectStore","readwrite");
+								var store = tx.objectStore("MyObjectStore");
 
-				open.onsuccess = function() {
-					console.log("Onsuccess");
-				    // Start a new transaction
-				    var db = open.result;
-				    var tx = db.transaction("MyObjectStore", "readwrite");
-				    var store = tx.objectStore("MyObjectStore");
-				    var index = store.index("NameIndex");
-				    
-				   /* for(d in data){
-				    	console.log(data[d].id +  data[d].name + data[d].age);
-				    	store.put({id: Number(data[d].id), name: data[d].name, gender: data[d].gender});
-				    }*/
-				    
-				   
-				    // Add some data
-				    //store.put({id: 12345, name: {first: "John", last: "Doe"}, age: 42});
-				    //store.put({id: 67890, name: {first: "Bob", last: "Smith"}, age: 35});
-				    
-				    // Query the data
-				    var getJohn = store.get(12345);
+								var j = 0;
 
-				    var getPerson = store.get(2074007);
-				    //var getPerson = index.get(["Haresh Bhagat", "MALE"]);
-				    var getBob = index.get(["Smith", "Bob"]);
+								store.openCursor().onsuccess = function(event) {
+									var cursor = event.target.result;
+									j++;
+									if (cursor) {
+										cursor.advance(Math.floor(Math.random() * (50000) + 1));
+										if(j==2){
+											//console.log(cursor.value);
+											tx.abort();
+										}
+									} 
+									else {
+										console.log('Entries all displayed.');
+									}
+								};
+						}
+					}
+					
+					$scope.readFn = function(){
+						for (var i = 0; i < 10000; i++) {
+							var d1 = new Date();
+							var n1 = d1.getTime();
+							$scope.readlat();
+							var d2 = new Date();
+							var n2 = d2.getTime();
+							var t = n2 - n1;
+							console.log("Record-" + i + "|" + t);
+						}
+					}
+					
+					$scope.writelat = function() {
+						//console.log("Write");
+						$http.get('../data/50000.json').success(
+								function(data) {
+									// console.log(data);
+									$scope.lala = data.length;
+									var open = indexedDB.open("MyDatabase", 1);
 
-				    getJohn.onsuccess = function() {
-				        console.log(getJohn.result.name.first);  // => "John"
-				    };
-				    
-				    getPerson.onsuccess = function() {
-				        console.log(getPerson.result);  // => "John"
-				    };
+									open.onupgradeneeded = function() {
+										//console.log("Onupgraded");
+										var db = open.result;
+										var store = db.createObjectStore(
+												"MyObjectStore", {
+													keyPath : "id"
+												});
+										// var store =
+										// db.createObjectStore("MyObjectStore2",
+										// {keyPath: "id"});
+										// var index =
+										// store.createIndex("NameIndex",
+										// ["name",
+										// "gender"]);
+									};
 
-				    getBob.onsuccess = function() {
-				        console.log(getBob.result.name.first);   // => "Bob"
-				    };
+									open.onsuccess = function() {
+										//console.log("Onsuccess");
+										// Start a new transaction
+										var db = open.result;
+										/*
+										 * var tx =
+										 * db.transaction("MyObjectStore",
+										 * "readwrite"); var store =
+										 * tx.objectStore("MyObjectStore");
+										 * 
+										 * var i = 0; for(d in data){
+										 * store.put({id: i, data: data[d].id});
+										 * i++; }
+										 */
 
-				    // Close the db when the transaction is done
-				    tx.oncomplete = function() {
-				        db.close();
-				    };
-				}
-			}
-		});
-		
-		// Kick off the update function
-		updateTime();
-	});
+										var tx = db.transaction(
+												"MyObjectStore", "readwrite");
+										var store = tx
+												.objectStore("MyObjectStore");
+
+										// var index = store.index("NameIndex");
+										// store.put({id: 0, data:data});
+										var i = 0;
+										for (d in data) {
+											var d1 = new Date();
+											var n1 = d1.getTime();
+											store.put({
+												id : Number(data[d].id),
+												data : data[d]
+											});
+											var d2 = new Date();
+											var n2 = d2.getTime();
+											++i;
+											var t = n2 - n1;
+											console.log("Record-" + i + "|" + t);
+										}
+									}
+
+								});
+					}
+
+					// Kick off the update function
+					// updateTime();
+				});
